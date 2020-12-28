f4db4daf40e5adbffd0bfebcda210d3216cb4ffd
A-2809 <66512619+A-2809@users.noreply.github.com>
2020-12-28 16:33:46 +0530

    Add files via upload

---
+++ makecackeMatrix.R
@@ -0,0 +1,16 @@
+## This function will create a "matrix" object that can cache its own inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
